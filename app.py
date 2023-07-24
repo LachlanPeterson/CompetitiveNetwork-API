13bf5939d34765e14f280a0c8574c17391b3e93c
LachlanPeterson <14383@coderacademy.edu.au>
2023-07-24 14:49:13 +1000

    Succesfully connected to the database server

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# protocol + adapter + username and password @ the port
+app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql+psycopg2://cn_dev:cndev123@localhost:5432'
+
+# Must be after config
+
+# Open connection to database
+db = SQLAlchemy(app)
+print(db.__dict__)
 
 @app.route('/')
 def index():
