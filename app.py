5dea4a8bf169ac4616a78453875b71466149054c
LachlanPeterson <14383@coderacademy.edu.au>
2023-07-24 15:46:35 +1000

    Added seed data from users

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from datetime import date
 
 app = Flask(__name__)
 
 # protocol + adapter + username and password @ the port
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://cn_dev:cndev123@localhost:5432/competitive_network'
 
-# Open connection to database
+# Open connection to database and intilized alchemy
 db = SQLAlchemy(app)
 
-# User Entity
+# User Entity model for Users
 class User(db.Model):
+  __tablename__ = 'users'
+  
   user_id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(16)) # name 16 characters max
   email = db.Column(db.String(50))
@@ -21,8 +24,31 @@ class User(db.Model):
 # Cli command to create tables
 @app.cli.command('create')
 def create_db():
+   db.drop_all()
    db.create_all()
    print('Testing table creation')
+
+# Cli command to seed tables
+@app.cli.command('seed')
+def seed_db():
+   users = User(
+      name = 'LachlanPeterson',
+      email = 'LachlanPeterson@gmail.com',
+      password = 'LPassword123',
+      is_admin = True,
+      date_created = date.today(),
+   )
+
+#    Truncate the User table
+   db.session.query(User).delete()
+
+#    Add the user to the session (transaction)
+   db.session.add(users)
+
+#    Commit the transaction to the database
+   db.session.commit()
+   print('Models seeded')
+
 
 @app.route('/')
 def index():
